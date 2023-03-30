691dbbadcfbb8927b3bf92da1496f4232507283c
montypython <montiback22@gmail.com>
2023-03-30 21:21:38 +0200

    Server_Patch

--- wallet/forms.py
+++ wallet/forms.py
@@ -7,7 +7,7 @@ class SignInForm(forms.Form):
 	password = forms.CharField(label = 'Password:', max_length=30, widget=forms.PasswordInput(attrs={'class': 'form-control'})
 )
 class SearchEngineForm(forms.Form):
-	search = forms.CharField(max_length=40, label='Coin:', widget=forms.EmailInput(attrs={'class':'form-control'}))
+	search = forms.CharField(max_length=40, label='Coin:', widget=forms.TextInput(attrs={'class':'form-control'}))
 	
 class AccountForm(ModelForm):
 	password = forms.CharField(widget=forms.PasswordInput)
